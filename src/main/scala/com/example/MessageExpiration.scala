d281c94f10e4c7d170a7e41379637f1ba0cac492
shigemk2 <i.am.shige@gmail.com>
2016-11-07 23:03:09 +0900

    Add trait ExpiringMessage

--- src/main/scala/com/example/MessageExpiration.scala
+++ src/main/scala/com/example/MessageExpiration.scala
@@ -2,5 +2,16 @@ package com.example
 
 import akka.actor._
 
+trait ExpiringMessage {
+  val occurredOn = System.currentTimeMillis()
+  val timeToLive: Long
+
+  def isExpired: Boolean = {
+    val elapsed = System.currentTimeMillis() - occurredOn
+
+    elapsed > timeToLive
+  }
+}
+
 object MessageExpirationDriver extends CompletableApp(3) {
 }
