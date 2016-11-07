8b40e2d89ddfd5f0a69225ddf9049353e383fa96
shigemk2 <i.am.shige@gmail.com>
2016-11-07 23:09:41 +0900

    Add PurchaseOrder

--- src/main/scala/com/example/MessageExpiration.scala
+++ src/main/scala/com/example/MessageExpiration.scala
@@ -1,6 +1,13 @@
 package com.example
 
+import java.util.Date
+import java.util.concurrent.TimeUnit
+
+import akka.actor.Actor.Receive
 import akka.actor._
+
+import scala.concurrent.duration.Duration
+import scala.util.Random
 
 trait ExpiringMessage {
   val occurredOn = System.currentTimeMillis()
@@ -16,4 +23,16 @@ trait ExpiringMessage {
 case class PlaceOrder(id: String, itemId: String, price: Double, timeToLive: Long) extends ExpiringMessage
 
 object MessageExpirationDriver extends CompletableApp(3) {
+}
+
+class PurchaseRouter(purchaseAgent: ActorRef) extends Actor {
+  val random = new Random((new Date()).getTime)
+
+  override def receive: Receive = {
+    case message: Any =>
+      val millis = random.nextInt(100) + 1
+      println(s"PurchaseRouter: delaying delivery of $message for $millis milliseconds")
+      val duration = Duration.create(millis, TimeUnit.MILLISECONDS)
+      context.system.scheduler.scheduleOnce(duration, purchaseAgent, message)
+  }
 }
